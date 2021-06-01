67d64aca856eb817b9a5263d80ebbf6703af7433
Kei <73867910+KeinShin@users.noreply.github.com>
2021-06-01 13:33:47 +0530

    UPGRADE

--- system/plugins/_alive_ea.py
+++ system/plugins/_alive_ea.py
@@ -10,13 +10,14 @@ from system.plugins import light
 from system.Config import Variable
 from system.Config.utils import  get_readable_time
 ping = get_readable_time((t.time() - t.time()))
-from system import (ALIVE_IMG, app, ALIVE_MESSAGE, OWNER, time, ttl, updates, self_hosted, MODE,      
-ALIVE_IMG
+from system import (ALIVE_IMG, COMMAND_HELP, app, ALIVE_MESSAGE, OWNER, time, ttl, updates, self_hosted, MODE
 )        # Easter OwO
+if ALIVE_IMG is None:
+    ALIVE_IMG = "https://telegra.ph/file/4e83650cf1e3e8c31c51b.mp4"
 
 
 
-@light.on(["alive", "black", "alv", f"{OWNER}"], sudo_ids=Variable.SUDO_IDS, file = "_alive_ea")
+@light.on(["alive", "black", "alv", f"{OWNER}", "awake"], sudo_ids=Variable.SUDO_IDS, file = "_alive_ea")
 async def alive(client, m):
     if ALIVE_MESSAGE is  None:
       text = f"""
@@ -31,6 +32,12 @@ async def alive(client, m):
       """
     else:
       text = ALIVE_MESSAGE
-    await app.send_document(m.chat.id, ALIVE_IMG, caption=text)
+    if ALIVE_IMG.endswith(".mp4"):
+          
+        await app.send_video(m.chat.id, ALIVE_IMG, caption=text)
+    elif ALIVE_IMG.endswith(".jpg") or ALIVE_IMG.endswith(".png"):
+        await app.send_photo(m.chat.id, ALIVE_IMG, caption=text)
+          
+
 
  

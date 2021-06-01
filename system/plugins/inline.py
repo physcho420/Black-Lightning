00ba60f63af237698e896de87ef432c6ac3b51ac
Kei <73867910+KeinShin@users.noreply.github.com>
2021-06-01 12:14:18 +0530

    Update inline.py

--- system/plugins/inline.py
+++ system/plugins/inline.py
@@ -10,7 +10,8 @@ from pyrogram.types import (   InlineKeyboardButton,
     InlineQueryResultArticle,
     InputTextMessageContent,
     InlineKeyboardMarkup,
-    InlineQuery
+    InlineQuery,
+    CallbackQuery
 
 
 
@@ -21,7 +22,28 @@ if ASISTANT_CMD_ROWS is None:
    number_of_rows_in_commands = 6
 
 
-
+mkp =   [
+            [
+                InlineKeyboardButton(
+                    text="Query", callback_data=f"fck_ask"
+                )
+            ],
+            [
+             InlineKeyboardButton(
+                    text="Urgent", callback_data=f"urgent"
+                )
+            ],
+            [
+             InlineKeyboardButton(
+                    text="Friend", callback_data=f"he_sucks"
+                )
+            ],
+            [
+             InlineKeyboardButton(
+                    text="Message", callback_data=f"message"
+                )
+            ],
+        ]
 
 
 
@@ -89,7 +111,7 @@ async def inline_handler(client, inline_query: InlineQuery):
                 ],
             )
     
-    if text == "Menu" and inline_query.id == on.id:
+    elif text == "Menu" and inline_query.from_user ['id'] == on.id:
         content = InputTextMessageContent("**Black Lightning Help Menu for User** [{}]({})".format(USER[1:],  f"tg://user?id={fuking_sucking.id}"))
 
 
@@ -134,35 +156,29 @@ async def inline_handler(client, inline_query: InlineQuery):
               
               
 
-    elif text == "Pm Protect":
+    elif text == "Pm Protect" and inline_query.from_user['id'] == a :
 
 
-        mkp =   [
-            [
-                InlineKeyboardButton(
-                    text="Query", callback_data=f"fck_ask"
-                )
-            ],
-            [
-             InlineKeyboardButton(
-                    text="Urgent", callback_data=f"urgent"
-                )
-            ],
-            [
-             InlineKeyboardButton(
-                    text="Friend", callback_data=f"he_sucks"
-                )
-            ],
-        ]
+
          
   
         await client.answer_inline_query(
             inline_query.id,
-            cache_time=1,
-            results=[InlineKeyboardMarkup(mkp)]
+            cache_time=0,
+            results=[
+                    (
+                        InlineQueryResultArticle(
+                            title="Help",
+                            input_message_content=InputTextMessageContent(
+                              Variable.PM_SECURITY_MSG,
+                            ),
+                            reply_markup=InlineKeyboardMarkup(mkp),
+                        )
+                    )
+                ],
+            )
 
         
-        )
     # elif  text == "Assistant Menu":
     #     fucking_sucking = await bot.get_me()
     #     text = inline_query.query
@@ -183,7 +199,7 @@ def blocked_user(name):
     blocked.append(name)
 
     
-@bot.on_message(filters.command(["alive"]) & filters.group & filters.incoming)
+@bot.on_message(filters.command(["alive"]) &  filters.incoming)
 async def alive(client, message):
 
        cap = f"""
@@ -207,9 +223,30 @@ async def alive(client, message):
        await bot.send_document(message.chat.id, ALIVE_IMG_ASSISTANT, caption = cap ,
        reply_markup=InlineKeyboardMarkup(wh))
  
+@bot.on_callback_query(filters.regex(pattern="message"))
 
+async def detailed(client, query: CallbackQuery):
+    if query.from_user.id  == (await app.get_me()).id:
+        await query.answer("If u can see this approve to disappove him/her")
+        return
 
+    m =[
+            [
+                InlineKeyboardButton(
+                    text="Menu", callback_data="menu"
+                )
+            ],
+        ]
+    await query.edit_message_text(f"Master Left Reason - {Variable.AFK_PM_MESSAGE}", reply_markup=m)
 
+@bot.on_callback_query(filters.regex(pattern="menu"))
+
+async def detailed(client, query: CallbackQuery):
+
+    if query.from_user.id  == (await app.get_me()).id:
+        await query.answer("Dis ain't for u tbh.")
+        return
+    await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(mkp))
 
 @bot.on_callback_query(filters.regex(pattern="sed_help_(.*)"))
 
@@ -237,46 +274,47 @@ async def query_hndr(client, message):
 
 
             # Thanks To Friday For This Deatiled Idea of detail button
-@bot.on_callback_query(filters.regex(pattern="detailed_(.*)(assistant|normal)"))
+@bot.on_callback_query(filters.regex(pattern="detailed_(.*)_(assistant|normal)"))
 
 async def detailed(client, message):
 
     light_pulu_name = message.matches[0].group(1)
     typea = message.matches[0].group(2)
     aa = f"{light_pulu_name}'s help"
-    try:
-            if light_pulu_name in COMMAND_HELP:
-                    lightning_help_strin = f"{language('Commands found in')} {light_pulu_name}:\n"
-                    lightning_help_strin += f"**🔺 NAME 🔺 :** {light_pulu_name} \n\n {COMMAND_HELP[aa]}"
-                    lightning_help_strin += "\n    " + light_pulu_name
-                    lightning_help_strin += "\n"
-                    o  =  [
-            [
-                InlineKeyboardButton(
-                    text="Delete", callback_data=f"krish_{light_pulu_name}"
-                )
-            ],
-            [
-             InlineKeyboardButton(
-                    text="Back", callback_data=f"back_{light_pulu_name}{typea}"
-                )
-            ],
-        ]
-                    await message.edit_message_text(
-                    text=lightning_help_strin,
-                    reply_markup=InlineKeyboardMarkup(o)
-                
-        )
-            else:
-                await message.answer(f"{language('No Deatiled Help For Command')} {light_pulu_name}", cache_time=0, show_alert=True)
-    
+    if typea == "normal":
+        try:
+                if light_pulu_name in COMMAND_HELP:
+                        lightning_help_strin = f"{language('Commands found in')} {light_pulu_name}:\n"
+                        lightning_help_strin += f"**🔺 NAME 🔺 :** {light_pulu_name} \n\n {COMMAND_HELP[aa]}"
+                        lightning_help_strin += "\n    " + light_pulu_name
+                        lightning_help_strin += "\n"
+                        o  =  [
+                [
+                    InlineKeyboardButton(
+                        text="Delete", callback_data=f"krish_{light_pulu_name}"
+                    )
+                ],
+                [
+                 InlineKeyboardButton(
+                        text="Back", callback_data=f"back_{light_pulu_name}_{typea}"
+                    )
+                ],
+            ]
+                        await message.edit_message_text(
+                        text=lightning_help_strin,
+                        reply_markup=InlineKeyboardMarkup(o)
+                    
+            )
+                else:
+                    await message.answer(f"{language('No Deatiled Help For Command')} {light_pulu_name}", cache_time=0, show_alert=True)
+        
      
                 
 
               
         
-    except KeyError:
-           await message.answer(f"{language('No Details')} U_U", cache_time=0, show_alert=True)
+        except KeyError:
+               await message.answer(f"{language('No Details')} U_U", cache_time=0, show_alert=True)
     
  
                 
@@ -301,7 +339,7 @@ async def query_hndr(client, message):
            mkp = [
             [
                 InlineKeyboardButton(
-                    text="Details", callback_data=f"detailed_{pg_no}{type_}"
+                    text="Details", callback_data=f"detailed_{pg_no}_{type_}"
                 )
             ],
             [
@@ -319,7 +357,7 @@ async def query_hndr(client, message):
            )
 @bot.on_callback_query(filters.regex(pattern="help_prev\((.+?)\)"))
 
-async def query_hndr(client, message): 
+async def query_hndr(client, message: CallbackQuery): 
     me = await app.get_me()
     if not  message.from_user.id ==  me.id:
                await message.answer("I said you cannot use anything. ఠ_ఠ", show_alert=True)
@@ -336,91 +374,14 @@ async def query_hndr(client, message):
 
 @bot.on_callback_query(filters.regex(pattern="lightning_is_here_cant_spam"))
 @owner
-async def lightning_is_better(client, message):
+async def lightning_is_better(client, message: CallbackQuery):
 
-    user = await app.get_users(int(message.chat.id))
     text1 = f"**Byy👋**!\n**You've been blocked have fun\n\n**If you think this is a mistake contact master via {g}**"
-    app.block_user(user.id)
-    blocked_user(user.first_name)
+    app.block_user(message.from_user.id)
+    blocked_user(message.from_user.id)
     await app.send_message(message.chat.id, text1)
 
 
-urgent = []
-
-
-
-
-def add_friend(name, id):
-    Friends.update({name: id})
-def add_urgent(name):
-    urgent.append(name)
-
-
-
-@bot.on_callback_query(filters.regex(pattern="urgent"))
-async def lightning_is_better(client, message):
-    a = await app.get_me()
-    user = await app.get_users(int(message.chat.id))
-
-    if user.is_self :
-        await message.answer("This command if for stranger not for the owner!", cache_time=0, show_alert=True)
-        return
-
-    name = user.first_name
-    bhat = user.status  
-    text1 = "**Hello User {},  master was last online on {}**\n**Kindly wait for him to be online :)** ".format(name, bhat)
-    await app.send_message(message.chat.id, text1)
-    await app.send_message(
-        Variable.LOGS_chat.id,
-        f"**Hello {USER}, [{name}]({user.id}) wants to dicuss something important!.**",
-    )
-    if user.is_deleted:
-     return
-    add_urgent(name)
-
-
-
-
-
-
-@bot.on_callback_query(filters.regex(pattern="he_sucks"))
-@owner
-async def lightning_is_better(client, message):
-    user =   await app.get_users(int(message.chat.id))
-    o = await app.get_me()
-    owner = await app.get_users(int(o.id
-
-    ))
-    user_id = user.id
-    await message.edit(f"**Hello {user.first_name} if u are friend kindly contact him via {g}**\n\n__{USER}:- was last online on__ {owner.last_online_date}")
-
-    
-    
-    
-    
-    
-    
-
-
-
-
-@bot.on_callback_query(filters.regex(pattern="fck_ask"))
-@owner
-async def lightning_is_better(client, message):
-    user =   await app.get_users(int(message.chat.id))
-    bot_id = await bot.get_me()
-    bot_id = bot_id.id
-    await message.edit
-    btn =InlineKeyboardMarkup([InlineKeyboardButton("Contact Him", url=f"tg://user?id={bot_id}")])
-
-    await app.send_message(
-        user.id,
-        f"Master is busy for some reason contact him via bot link given below",
-        reply_markup=btn,
-    )
-
-
-          
 
 
 
@@ -450,7 +411,7 @@ async def chill(client, message):
 
 
 
-@bot.on_callback_query(filters.regex(pattern="back_(.*)(assitant|normal)"))
+@bot.on_callback_query(filters.regex(pattern="back_(.*)_(assitant|normal)"))
 async def ho(client, message):
     o=message.matches[0].group(1)
     if message.matches[0].group(2) == "assistant":
@@ -495,7 +456,7 @@ def help_menu(pg_num, setv, prefix, alive: bool =  False):
             text=f"X {x} X",
             callback_data=f"{data}{x}{type_}"
         )
-        for x in sortsed 
+        for x in sorted(sortsed.keys())
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
@@ -513,7 +474,7 @@ def help_menu(pg_num, setv, prefix, alive: bool =  False):
                 ),
                 InlineKeyboardButton(
                     text="Next",
-                    callback_data="{}_next({})".format(
+                    callback_data="{}{}({})".format(
                         prefix, help2, pg_num
                     ),
                 ),
@@ -566,7 +527,7 @@ async def lightning_pugins_query_hndlr(client, lightning):
             buttons = help_menu(
                 lightning_page - 1, ASSISTANT_HELP, "help"  # pylint:disable=E0602
             )
-            await lightning.edit_message_text(reply_markup=buttons)
+            await lightning.edit_message_text(txt="**Commands Listed Choose which one to know**",reply_markup=buttons)
 
 import io
 # from system.sqls.bot_sql import *
@@ -588,9 +549,90 @@ async def ass_pugins_query_hndlr(client, lightning):
         buttons = help_menu(
             lightning_page + 1, ASSISTANT_HELP, "help"  # pylint:disable=E0602
         )
-        await lightning.edit_message_text(reply_markup=buttons)
+        await lightning.edit_message_text("**Commands Listed Choose which one to know**", reply_markup=buttons)
 
 
 
 
 
+blocked =[]
+def blocked_user(name):
+    blocked.append(name)
+@bot.on_callback_query(filters.regex(pattern="lightning_is_here_cant_spam"))
+@owner
+async def lightning_is_better(client, message):
+
+    text1 = f"**Byy👋**!\n**You've been blocked have fun\n\n**If you think this is a mistake contact master via {g}**"
+    await app.block_user(message.from_user.id)
+    blocked_user(message.from_user.id)
+    await app.send_message(message.chat.id, text1)
+
+
+
+
+urgent = []
+
+
+Friends = {}
+
+def add_friend(name, id):
+    Friends.update({name: id})
+def add_urgent(name):
+    urgent.append(name)
+
+
+
+@bot.on_callback_query(filters.regex(pattern="urgent"))
+async def lightning_is_better(client, message):
+    a = await app.get_me()
+    user = await app.get_users(int((await app.get_me()).id))
+
+    if message.from_user.id == (await app.get_me()).id :
+        await message.answer("This command if for stranger not for the owner!", cache_time=0, show_alert=True)
+        return
+    USER = OWNER
+
+    name = user.first_name
+    bhat = user.last_online_date  
+    text1 = "**Hello User `{}`,  master was last online on `{}`**\n**Kindly wait for him to be online :)** ".format(name, bhat)
+    await message.edit_message_text(text1)
+    await app.send_message(
+        Variable.LOGS_CHAT_ID,
+        f"**Hello {USER}, [{name}]({user.id}) wants to dicuss something important!.**",
+    )
+    if user.is_deleted:
+     return
+    add_urgent(name)
+
+
+
+
+
+
+@bot.on_callback_query(filters.regex(pattern="he_sucks"))
+async def lightning_is_better(client, message):
+    user =   await app.get_users(int(message.from_user.id))
+    o = await app.get_me()
+    owner = await app.get_users(int(o.id
+
+    ))
+    user_id = user.id
+    await message.edit_message_text(f"**Hello {user.first_name} if u are friend kindly contact him via {g}**\n\n__{USER}:- was last online on__ {owner.last_online_date}")
+
+    
+    
+    
+    
+    
+    
+
+
+
+
+@bot.on_callback_query(filters.regex(pattern="fck_ask"))
+async def lightning_is_better(client, message: CallbackQuery):
+    user =   await app.get_users(int(message.from_user.id))
+    bot_id = await bot.get_me()
+    bot_id = bot_id.id
+    await message.edit_message_text('Master is busy for some reason contact him via bot link given below',
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Contact Him", url=f"t.me/{Variable.TG_BOT_USER_NAME}")]]))

17f23f27796685fc2efcd58edcc13947ebc4b5f2
Kei <73867910+KeinShin@users.noreply.github.com>
2021-06-08 19:17:37 +0530

    Fix for circular import

--- system/datas_mongo/__init__.py
+++ system/datas_mongo/__init__.py
@@ -1,14 +1,15 @@
 from pymongo import *
 from pymongo.errors import ConfigurationError
 
-from system.Config import Variable
+import  system.Config
+
 import logging
 try:
 
 
 
          
- client =  MongoClient(Variable.MONGO_DB_URL)
+ client =  MongoClient(system.Config.Variable.MONGO_DB_URL)
 except ConfigurationError:
     logging.info("Mongo DB URL Incorrect!, check whats wrong with it ")
 sed = client['test']
